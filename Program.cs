d01addd4ffe912fc07efbb2aea54cbca3dfa0385
Mohamed Attya <ATTYA@DESKTOP-S6J1JOP>
2025-02-17 03:52:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment2_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
